71857d3221d8b7111918ddb9d940b78811111334
Ben Sully <ben@bsull.io>
2018-10-08 22:57:20 +0100

    Document CLI better

--- src/cli.rs
+++ src/cli.rs
@@ -19,27 +19,59 @@ pub struct Opt {
 #[derive(Debug, StructOpt)]
 pub enum Command {
 
-    #[structopt(name = "split", about = "Split dataset into two or more files for test/train/validation sets")]
+    #[structopt(
+        name = "split",
+        about = "Split dataset into two or more files for test/train/validation sets",
+    )]
     Split(Split),
 }
 
 #[derive(Debug, StructOpt)]
 pub struct Split {
-    #[structopt(short = "r", long = "rows", required_unless = "prop_splits", conflicts_with = "prop_splits", help = "Specify splits by number of rows", use_delimiter = true)]
+    #[structopt(
+        short = "r",
+        long = "rows",
+        required_unless = "prop_splits",
+        conflicts_with = "prop_splits",
+        help = "Specify splits by number of rows",
+        use_delimiter = true,
+    )]
     pub row_splits: Vec<RowSplit>,
 
-    #[structopt(short = "p", long = "prop", required_unless = "row_splits", help = "Specify splits by proportion of rows", use_delimiter = true)]
+    #[structopt(
+        short = "p",
+        long = "prop",
+        required_unless = "row_splits",
+        help = "Specify splits by proportion of rows",
+        use_delimiter = true,
+    )]
     pub prop_splits: Vec<ProportionSplit>,
 
-    #[structopt(short = "c", long = "chunk-size")]
+    #[structopt(
+        short = "c",
+        long = "chunk-size",
+        help = "Maximum number of rows per output chunk",
+    )]
     pub chunk_size: Option<u64>,
 
-    #[structopt(short = "t", long = "total-rows")]
+    #[structopt(
+        short = "t",
+        long = "total-rows",
+        help = "Number of rows in input file. Used for progress when using proportion splits",
+    )]
     pub total_rows: Option<u64>,
 
-    #[structopt(short = "s", long = "seed")]
+    #[structopt(
+        short = "s",
+        long = "seed",
+        help = "RNG seed, for reproducibility",
+    )]
     pub seed: Option<u64>,
 
-    #[structopt(parse(from_os_str))]
+    #[structopt(
+        parse(from_os_str),
+        raw(requires_if = r#""-", "filename""#),
+        help = "Data to split, optionally gzip compressed. If '-', read from stdin",
+    )]
     pub input: PathBuf,
 }
